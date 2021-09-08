4bf3b84a8b8df3ab583f6e2e58cfdead43ed45b4
TheDucky <90206823+TheDucky@users.noreply.github.com>
2021-09-08 17:23:38 +0530

    Add files via upload

---
+++ terminal-CheatSheet/open.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "-------------------------------------"
+echo "| thank you for using my CheatSheet |"
+echo "-------------------------------------"
+nano CheatSheet.txt
+xdg-open CheatSheet.txt
