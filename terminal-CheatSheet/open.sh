a10fb45ba448b5636f374802a080e9cb036892a6
TheDucky <90206823+TheDucky@users.noreply.github.com>
2021-09-29 19:35:37 +0530

    Delete open.sh

--- terminal-CheatSheet/open.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "-------------------------------------"
-echo "| thank you for using my CheatSheet |"
-echo "-------------------------------------"
-nano CheatSheet.txt
-xdg-open CheatSheet.txt
